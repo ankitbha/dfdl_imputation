fda8552cbf1e1433cb93bb413f0c6527bf32f02f
Joshua Weiner <joshuaweinernyc@gmail.com>
2024-06-06 21:05:43 -0400

    commit

--- experiment_utils.py
+++ experiment_utils.py
@@ -12,6 +12,16 @@ def run_genie(x, target_file):
     roc_score = roc_auc_score(gt.flatten(), rescaled_vim.flatten())
     return roc_score
 
+from knn_smoothing.knn_smooth import knn_smoothing
+def run_knn(data, k, save_path, it, file_extension='', target_file=None):
+    data[data == 0] = 1e-4
+    data = np.float64(data)
+    data[data == np.nan] = 1e-4
+    result = knn_smoothing(data, k)
+    np.save(save_path + "y_hat_knn" + file_extension, result)
+    roc = run_genie(result, target_file)
+    return roc, it  
+
 def run_scvi(data, save_path, it, file_extension='', target_file=None):
     adata = anndata.AnnData(data)
     scvi.model.SCVI.setup_anndata(adata)
